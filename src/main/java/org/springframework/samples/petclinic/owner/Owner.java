fce1ecc0f6f3013b294dd3099a6b4c6dc2549cc3
surjan-kshetri <36111454+surjan-kshetri@users.noreply.github.com>
2020-02-05 21:10:22 -0500

    Update Owner.java

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -46,7 +46,7 @@ import org.springframework.samples.petclinic.model.Person;
 @Table(name = "owners")
 public class Owner extends Person {
 
-	@Column(name = "address")
+	@Column(name = "Location")
 	@NotEmpty
 	private String address;
 
